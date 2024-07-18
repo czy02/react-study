9fc21087588ad3586dfe08fb75715a093683c0bb
czy02 <2382289558@qq.com>
2024-07-18 14:15:00 +0800

    别名路径配置

--- react-bill-test/src/index.js
+++ react-bill-test/src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import sum from "@/test";
+
+const to = sum(1, 2);
+
+console.log(to);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
