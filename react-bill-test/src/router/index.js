43806922f33b92f7b6e47850dffb40b06a1c0db3
czy02 <2382289558@qq.com>
2024-07-18 14:40:27 +0800

    路由配置

---
+++ react-bill-test/src/router/index.js
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "@/pages/Layout";
+import New from "@/pages/New";
+import Month from "@/pages/Month";
+import Year from "@/pages/Year";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        index: true,
+        element: <Month></Month>,
+      },
+      {
+        path: "year",
+        element: <Year></Year>,
+      },
+    ],
+  },
+  {
+    path: "/new",
+    element: <New></New>,
+  },
+]);
+
+export default router;
