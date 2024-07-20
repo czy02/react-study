01905c418004354bb422b28f63ed5db42952cb59
czy02 <2382289558@qq.com>
2024-07-20 11:26:48 +0800

    月度账单： 展示收入支出结余

--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -18,10 +18,25 @@ const Month = () => {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs(new Date()).format("YYYY | MM");
   });
+  const [currentMonthList, setCurrentMonthList] = useState([]);
+  const monthResult = useMemo(() => {
+    const pay = currentMonthList
+      .filter((item) => item.type === "pay")
+      .reduce((a, c) => a + c.money, 0);
+    const income = currentMonthList
+      .filter((item) => item.type === "income")
+      .reduce((a, c) => a + c.money, 0);
+    return {
+      pay,
+      income,
+      total: pay + income,
+    };
+  }, [currentMonthList]);
   const onConfirm = (date) => {
     setDateVisible(false);
     const formatDate = dayjs(date).format("YYYY | MM");
     setCurrentDate(formatDate);
+    setCurrentMonthList(monthGroup[formatDate]);
   };
   return (
     <div className="monthlyBill">
@@ -43,15 +58,15 @@ const Month = () => {
           {/* 统计区域 */}
           <div className="twoLineOverview">
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{monthResult.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.income.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{monthResult.total.toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
