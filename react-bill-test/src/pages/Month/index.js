74ce2f1b3ee1cedad541a2ac166b9f81f95079a4
czy02 <2382289558@qq.com>
2024-07-20 10:07:26 +0800

    静态结构搭建

--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -1,5 +1,46 @@
+import { NavBar, DatePicker } from "antd-mobile";
+import "./index.scss";
 const Month = () => {
-  return <div>Month</div>;
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backArrow={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date">
+            <span className="text">2024 | 7月账单</span>
+            {/* 思路：根据弹框打开状态控制expand类名是否存在 */}
+            <span className="arrow expand"></span>
+          </div>
+          {/* 统计区域 */}
+          <div className="twoLineOverview">
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={false}
+            max={new Date()}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Month;
