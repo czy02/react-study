7b2bbbfbce3cd81f68e51978358d8d4c885c39ad
czy02 <2382289558@qq.com>
2024-07-20 10:24:22 +0800

    月度账单，切换时间显示

--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -2,11 +2,18 @@ import { NavBar, DatePicker } from "antd-mobile";
 import "./index.scss";
 import { useState } from "react";
 import classNames from "classnames";
+import dayjs from "dayjs";
 const Month = () => {
   // 控制弹框打开关闭
   const [dateVisible, setDateVisible] = useState(false);
-  const onConfirm = () => {
+
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs(new Date()).format("YYYY | MM");
+  });
+  const onConfirm = (date) => {
     setDateVisible(false);
+    const formatDate = dayjs(date).format("YYYY | MM");
+    setCurrentDate(formatDate);
   };
   return (
     <div className="monthlyBill">
@@ -18,7 +25,7 @@ const Month = () => {
           {/* 时间切换区域 */}
           <div className="date">
             <span className="text" onClick={() => setDateVisible(true)}>
-              2024 | 7月账单
+              {currentDate + ""}月账单
             </span>
             {/* 思路：根据弹框打开状态控制expand类名是否存在 */}
             <span
